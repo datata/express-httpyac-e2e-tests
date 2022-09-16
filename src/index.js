ccb5fa46bbe0a33fa2287c07c90bc30d5af4a186
datata <dani@danitarazona.dev>
2022-09-16 16:25:22 +0200

    feat: get tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/test', (req, res) => {
+    return res.status(200).json(
+        {
+            success: "true",
+            message: "Getting test"
+        }
+    )
+});
+
 app.listen(3000, () => console.log(`server listening on port 3000`));
